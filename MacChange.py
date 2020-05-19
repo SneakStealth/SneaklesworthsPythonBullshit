03b2d683d30f08c572dbe4262e58f8ccaa95a0b9
SneakStealth <spriggsbrandond@gmail.com>
2020-05-18 20:57:07 -0600

    Revert "The Final Update(TM)"
    This reverts commit dff3c9f56d119e9fb0d9c13dc8afb3a5db26de88.

--- MacChange.py
+++ MacChange.py
@@ -1,33 +1,93 @@
-#!/usr/bin/env python
-#Python 3 only fucker. Until can import input
+#!/usr/bin/env python3
+#==========================================Information.=========================================================
+# Sneaklesworth Heavy Industries, 2020
+
+#Comments for noobs.
+
+
+#Python 3 only fucker. Until can import input, but people say its better to just use Python3 so, python3, fucker.
+
+#If a python script (written on windows) fails to execute after chmod +X and ./script.py then
+#The reason it fails is because of line endings or some bullshit being different on windows.
+#sudo apt-get install dos2unix
+#dos2unix /pathto/script.py
+#you can also do unix2dos to do the reverse if you dev on unix and run on windows
+
+##=============================================================================================================
+
 import subprocess #Allows for interfacing with shell and running system commands.
 import optparse #module to allow for parsing from command line. This will let arguments come into play like -h -i -m
+import re #fucking regex
+import sys #lol bytes-like strings
+#==================================FUNCTION DEFINITIONS=======================================================
 
-parser = optparse.OptionParser()
+def change_mac(interface, new_mac): #Defines the function to change the mac, so you can use it elsewhere easily
+
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)  # This is a simple print just for sanity sake so you know what the address changes to as output
+    subprocess.call(["ip", "link", "set", "dev", interface, "down"])  # Uses the ip command to take the interface down
+    subprocess.call(["ip", "link", "set", "dev", interface, "address", new_mac])  # uses the IP command to assign a new address to the interface
+    subprocess.call(["ip", "link", "set", "dev", interface, "up"])  # uses the ip command to bring the interface back up.
+
+def get_arguments():
+    parser = optparse.OptionParser()  # An object [parser] that can handle input using arguments via optparse
+    parser.add_option("-i", "--interface", dest="interface",help="Interface that you want to change the MAC for")  # Option for interface
+    parser.add_option("-m", "--mac", dest="mac",help="The Mac Address you want to change to.")  # Option for Mac Address
+    (options, arguments) = parser.parse_args() #sends the arguments outside the function when the function is called
+    if not options.interface: #If options.interface does not contain value then
+        parser.error("[-] Please specify an interface. Use --help for more information.") #Prints the string, and exits
+    elif not options.mac: #Else, if options.mac does not contain value then
+        parser.error("[-] Please specify a MAC address. Use --help for more information.") #Prints the string, and exits
+    return options
+
+def mac_search():
+    ip_output = subprocess.check_output(["ip", "link", "show", options.interface]).decode(sys.stdout.encoding)  # I had to think about this. Thanks, mr linux.
+    # print(ip_output)
+    mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ip_output)
+
+    if mac_search_result:
+        print(mac_search_result.group(0))
+    else:
+        print("Could not get MAC address")
 
 
-interface = input("Interface > ")
-new_mac = input("Mac > ")
-print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+#=====================================STUFF===============================================================
 
 
-# subprocess.call("sudo ip link set dev " + interface +" down" , shell=True)
+options = get_arguments() #Calls get_arguments function
+change_mac(options.interface, options.mac) #Calls the function defined above.
+mac_search()#Cool new way to report the new mac address from the ip command. Might be useful later(TM) Look at the function.
+
+
+#=================================OLD GARBAGE===========================================================
+# This section is commented out due to being replaced with  functions above. Functions are the best
+
+
+#The 4 lines below are replaced with the get_arguments function.
+# parser = optparse.OptionParser() #An object [parser] that can handle input using arguments via optparse
+# parser.add_option("-i", "--interface", dest="interface", help="Interface that you want to change the MAC for") #Option for interface
+# parser.add_option("-m", "--mac", dest="mac", help="The Mac Address you want to change to.") #Option for Mac Address
+# (options, arguments) = parser.parse_args() #method which allows the parser object to understand what the user has input. It can return args to a variable. In this case it returns args and values
+
+
+
+
+#subprocess.call("sudo ip link set dev " + interface +" down" , shell=True)
 # subprocess.call("sudo ip link set dev " + interface +" address " + new_mac , shell=True)
 # subprocess.call("sudo ip link set dev " + interface + " up" , shell=True)
-#
-# The above 3 commands are insecure, they lack input validation in any sense.
-# If a malicious user say for the intercface input did eth0;sudo rm -r -f / --no-preserve-root
-# bad things would happen so dont use it like this
 
-subprocess.call(["ip", "link", "set", "dev", interface, "down"])
-subprocess.call(["ip", "link", "set", "dev", interface, "address", new_mac])
-subprocess.call(["ip", "link", "set", "dev", interface, "up"])
+# The above 3 commands are insecure, they lack input validation in any sense.
+# If a malicious user say for the interface input did eth0;sudo rm -r -f / --no-preserve-root
+# bad things would happen so don't use it like this
+
+
+# print("[+] Changing MAC Address for " + interface + " to " + new_mac) #This is a simple print just for sanity sake so you know what the address changes to as output
+#
+# subprocess.call(["ip", "link", "set", "dev", interface, "down"]) #Uses the ip command to take the interface down
+# subprocess.call(["ip", "link", "set", "dev", interface, "address", new_mac]) #uses the IP command to assign a new address to the interface
+# subprocess.call(["ip", "link", "set", "dev", interface, "up"]) #uses the ip command to bring the interface back up.
 
 #These set of commands work in a similar way, except it puts the command into a list instead of a single string
 #which disallows a malicious user from injecting a different shell command. use this for subprocess
 
 
-
-
-
-
+#===========================================================================================================
